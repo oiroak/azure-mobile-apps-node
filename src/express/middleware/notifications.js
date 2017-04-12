bb0108477ed3e25b5cc4f1b96a1d1eb59fffb4a0
danderson00 <danderson00@gmail.com>
2017-04-12 13:31:36 -0700

    Remove string template literal

--- src/express/middleware/notifications.js
+++ src/express/middleware/notifications.js
@@ -34,7 +34,7 @@ module.exports = function (configuration) {
 
     function respond(req, res, next) {
         var action = req.method === 'PUT' ? 'registration' : 'deletion';
-        log.verbose(`Received push notification installation ${action} request. Returning stubbed response (registration only occurs when hosted on Azure).`);
+        log.verbose('Received push notification installation ' + action + ' request. Returning stubbed response (registration only occurs when hosted on Azure).');
         res.status(204).end();
     }
 };
